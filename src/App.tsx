100e3c8362b666778e963e34f555e517879ab694
FurusawaHidetora <s2412915@u.tsukuba.ac.jp>
2025-09-22 00:33:31 +0900

    9/21作業

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import "./App.css";
 import { About } from "./section/about";
 import { News } from "./section/news";
 import { Topics } from "./section/topics";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">フルサワークス</header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <header
+        className="App-header"
+        style={{
+          backgroundColor: "black",
+          color: "white",
+          width: "100%",
+          height: "50px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <div style={{ position: "absolute", left: "10px" }}>フルサワークス</div>
+      </header>
       <Topics />
       <About />
       <News />
--- src/section/topics.tsx
+++ src/section/topics.tsx
@@ -1,21 +1,51 @@
 export const Topics = () => {
   return (
-    <div className="topics">
-      <div>
-        <div className="big-topic">
-          <button onClick={() => (window.location.href = "random")}>
-            <img src="random" alt="random" />
-          </button>
-        </div>
-        <div className="small-topic">
-          <button onClick={() => (window.location.href = "random")}>
-            <img src="random" alt="random" />
-          </button>
-          <button onClick={() => (window.location.href = "random")}>
-            <img src="random" alt="random" />
-          </button>
-        </div>
+    <div
+      className="topics"
+      style={{
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <div>TOPICS</div>
+      <div
+        className="big-topic"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <button onClick={() => (window.location.href = "random")}>
+          <img
+            src="/article/IMG_0992.jpg"
+            alt="IMG_0992"
+            style={{ width: "800px" }}
+          />
+        </button>
       </div>
+      <div
+        className="small-topic"
+        style={{ display: "flex", flexDirection: "row" }}
+      >
+        <button onClick={() => (window.location.href = "random")}>
+          <img
+            src="/article/IMG_0025.jpg"
+            alt="IMG_0025"
+            style={{ width: "400px" }}
+          />
+        </button>
+        <button onClick={() => (window.location.href = "random")}>
+          <img
+            src="/article/IMG_9965.jpg"
+            alt="IMG_9965"
+            style={{ width: "400px" }}
+          />
+        </button>
+      </div>
+
       <button onClick={() => (window.location.href = "WORKS")}>
         VIEW MORE
       </button>
