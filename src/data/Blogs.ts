21d718e71ff9c592e644ce6881017cc706c77b5c
FurusawaHidetora <s2412915@u.tsukuba.ac.jp>
2025-10-30 01:50:02 +0900

    初記事の公開

--- src/data/Blogs.ts
+++ src/data/Blogs.ts
@@ -1,10 +1,10 @@
 // 記事のメタデータ一覧
 export const blogMetadata = [
   {
-    id: "sanpo",
-    title: "【初記事】散歩のすすめ",
-    date: "2025-10-26",
-    markdownPath: `${process.env.PUBLIC_URL}/articles/sanpo.md`,
+    id: "first-post",
+    title: "【初記事】サイトを公開したよ！",
+    date: "2025-10-30",
+    markdownPath: `${process.env.PUBLIC_URL}/articles/first-post.md`,
   },
   // 他の記事...
 ];
--- src/section/about.tsx
+++ src/section/about.tsx
@@ -30,7 +30,7 @@ export const About = () => {
             aspectRatio: "1/1",
             objectFit: "cover",
           }}
-          src={`${process.env.PUBLIC_URL}/about/profile_photo.jpg`}
+          src={`${process.env.PUBLIC_URL}/about/profile_photo.jpeg`}
           alt="my-photo"
         />
       </div>
@@ -40,7 +40,7 @@ export const About = () => {
       <div>
         最近やっと野菜が美味しく食べられるようになってきた。粘土をこねてみたり、酒を飲んでみたりと自由な生活を送っている。
         好きな技はメシアライザー(全体回復、状態異常も治す)、べノンザッパー(敵全体に物理攻撃、確率で毒を付与)。なんか名前がかっこいいので。全体技なのもポイントが高い。
-        髪型はデフォルトが坊主だがたまに伸ばすこともある。暖房に弱い。
+        髪型は坊主がデフォルトだがたまに伸ばすこともある。暖房に弱い。
       </div>
     </div>
   );
